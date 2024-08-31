2d1235c4b3ce8c4519b44e7af878ad135c53dcfc
tridev1004 <85545474+tridev1004@users.noreply.github.com>
2024-08-31 18:20:00 +0530

    Complete

--- app/page.js
+++ app/page.js
@@ -1,11 +1,18 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const Router=useRouter();
+  const routing=()=>{
+    Router.push("/dashboard")
+  }
   return (
    <div>
     <h1>Home Page</h1>
-    <Button>Subscribe</Button>
+    <Button onClick={routing}>Subscribe</Button>
    </div>
   );
 }
