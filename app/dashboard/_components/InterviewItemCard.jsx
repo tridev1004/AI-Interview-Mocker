e6bdd467fc76f358e07bb95a05367ba6a583673c
tridev1004 <85545474+tridev1004@users.noreply.github.com>
2025-02-06 09:26:36 +0530

    FIX: delete functionality

--- app/dashboard/_components/InterviewItemCard.jsx
+++ app/dashboard/_components/InterviewItemCard.jsx
@@ -1,6 +1,12 @@
 import { Button } from '@/components/ui/button'
+import { db } from '@/utils/db';
+import { MockInterview } from '@/utils/schema';
+import { eq } from 'drizzle-orm';
+import { pgTable } from 'drizzle-orm/pg-core';
+import { Trash2 } from 'lucide-react';
 import { useRouter } from 'next/navigation'
 import React from 'react'
+import { toast } from 'sonner';
 
 const InterviewItemCard = ({interview}) => {
     const router=useRouter();
@@ -11,6 +17,19 @@ const InterviewItemCard = ({interview}) => {
     const onFeedbackPress=()=>{
         router.push('/dashboard/interview/'+interview.mockId+"/feedback")
     }
+    const deleteInterview=async(interview)=>{
+
+      await deleteMockInterviewById(interview.id)
+
+
+      
+    }
+
+async function deleteMockInterviewById(id) {
+  await db.delete(MockInterview).where(eq(MockInterview.id, id));
+  toast.success('Mock interview deleted successfully');
+  console.log(`Deleted mock interview with ID: ${id}`);
+}
   return (
     <div className='p-3 border rounded-lg shadow-sm'>
         <h2 className='font-bold text-primary '>{interview?.jobPosition}</h2>
@@ -19,6 +38,7 @@ const InterviewItemCard = ({interview}) => {
     <div className="flex justify-between gap-5 mt-2">
     <Button onClick={onFeedbackPress} size="sm" variant="outline" className="w-full">Feedback</Button>
     <Button onClick={onstart} size="sm" className="w-full">Start</Button>
+    <Button onClick={deleteInterview(interview)} size="sm" variant="outline" className="w-full">Delete</Button>
     </div>
 
     </div>
--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <Toaster />
+          <Toaster  richColors position="top-right" />
           {children}
         </body>
       </html>
