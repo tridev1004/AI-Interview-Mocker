692991a7a77a40000955dc0c4becdddbfbb72284
tridev1004 <85545474+tridev1004@users.noreply.github.com>
2025-02-06 09:40:45 +0530

    FIX: moment use

--- app/dashboard/_components/InterviewItemCard.jsx
+++ app/dashboard/_components/InterviewItemCard.jsx
@@ -4,6 +4,7 @@ import { MockInterview } from '@/utils/schema';
 import { eq } from 'drizzle-orm';
 import { pgTable } from 'drizzle-orm/pg-core';
 import { Trash2 } from 'lucide-react';
+import moment from 'moment';
 import { useRouter } from 'next/navigation'
 import React from 'react'
 import { toast } from 'sonner';
@@ -28,17 +29,16 @@ const InterviewItemCard = ({interview}) => {
 async function deleteMockInterviewById(id) {
   await db.delete(MockInterview).where(eq(MockInterview.id, id));
   toast.success('Mock interview deleted successfully');
-  console.log(`Deleted mock interview with ID: ${id}`);
 }
   return (
     <div className='p-3 border rounded-lg shadow-sm'>
         <h2 className='font-bold text-primary '>{interview?.jobPosition}</h2>
         <h2 className='text-sm text-gray-600'>{interview.jobExperience}Years of Experience</h2>
-        <h2 className='text-xs text-gray-400'>Created At:{interview.createdAt}</h2>
+        <h2 className='text-xs text-gray-400'>Created At:{moment(interview.createdAt).format('DD-MM-YYYY HH:mm:ss')}</h2>
     <div className="flex justify-between gap-5 mt-2">
     <Button onClick={onFeedbackPress} size="sm" variant="outline" className="w-full">Feedback</Button>
     <Button onClick={onstart} size="sm" className="w-full">Start</Button>
-    <Button onClick={deleteInterview(interview)} size="sm" variant="outline" className="w-full">Delete</Button>
+    <Button onClick={()=>deleteInterview(interview)} size="sm" variant="outline" className="w-full"><Trash2 className='text-red-500'/></Button>
     </div>
 
     </div>
--- app/dashboard/interview/[interviewId]/start/_component/QuestionsSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_component/QuestionsSection.jsx
@@ -12,6 +12,7 @@ const QuestionsSection = ({ mockInterviewQuestion=[], activeQuestionIndex }) => 
         toast.error('Sorry,Your Browser does not support text to Speech')
        }
     }
+    console.log(mockInterviewQuestion)
   return (
     <div className="p-5 my-10 border rounded-lg">
       <div className="grid gap-5 gird-cols-2 md:grid-cols-3 lg:grid-cols-4">
